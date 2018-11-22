22a799094f2f21f9e0cfc16e477d37d778c37c0e
wwagner4 <wwagner4@ben>
2018-11-22 18:26:48 +0100

    constants in round obj

--- src/simu/RoundObj.java
+++ src/simu/RoundObj.java
@@ -3,13 +3,15 @@ package simu;
 import java.awt.Graphics2D;
 
 public class RoundObj extends Obj {
+    
+    private static final int WIDTH = 10;
 
     public RoundObj(int x, int y) {
         super(x, y);
     }
 
     public void paint(Graphics2D g) {
-        g.fillOval(getX() - 50, getY()- 50, 100, 100);
+        g.fillOval(getX() - WIDTH, getY()- WIDTH, WIDTH * 2, WIDTH * 2);
     }
 
     
